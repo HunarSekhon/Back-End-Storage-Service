df02b9e682b4dfedde1820887aad1bfa880b14de
Jason Hau <jch26@sfu.ca>
2016-04-04 16:37:16 -0700

    Add PushSever.cpp Skeleton

--- PushServer.cpp
+++ PushServer.cpp
@@ -1,10 +1,132 @@
+  /*
+ Push Server code for CMPT 276, Spring 2016.
+ */
+
+#include <exception>
 #include <iostream>
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
 
-using namespace std;
+#include <cpprest/base_uri.h>
+#include <cpprest/http_listener.h>
+#include <cpprest/json.h>
 
-// TODO
-int main() {
-	cout << "PushServer" << endl;
-	cout << "TODO" << endl;
-	return 0;
+#include <pplx/pplxtasks.h>
+
+#include <was/common.h>
+#include <was/storage_account.h>
+#include <was/table.h>
+
+#include "TableCache.h"
+#include "make_unique.h"
+
+#include "azure_keys.h"
+
+#include "ServerUtils.h"
+
+#include "ClientUtils.h"
+
+using azure::storage::cloud_storage_account;
+using azure::storage::storage_credentials;
+using azure::storage::storage_exception;
+using azure::storage::cloud_table;
+using azure::storage::cloud_table_client;
+using azure::storage::edm_type;
+using azure::storage::entity_property;
+using azure::storage::table_entity;
+using azure::storage::table_operation;
+using azure::storage::table_query;
+using azure::storage::table_query_iterator;
+using azure::storage::table_result;
+
+using pplx::extensibility::critical_section_t;
+using pplx::extensibility::scoped_critical_section_t;
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::getline;
+using std::make_pair;
+using std::pair;
+using std::string;
+using std::unordered_map;
+using std::vector;
+
+using web::http::http_headers;
+using web::http::http_request;
+using web::http::methods;
+using web::http::status_code;
+using web::http::status_codes;
+using web::http::uri;
+
+using web::json::value;
+
+using web::http::experimental::listener::http_listener;
+
+using prop_vals_t = vector<pair<string,value>>;
+
+constexpr const char* def_url = "http://localhost:34574";
+
+const string create_table {"CreateTableAdmin"};
+const string delete_table {"DeleteTableAdmin"};
+
+const string read_entity_admin {"ReadEntityAdmin"};
+const string update_entity_admin {"UpdateEntityAdmin"};
+const string delete_entity_admin {"DeleteEntityAdmin"};
+
+const string read_entity_auth {"ReadEntityAuth"};
+const string update_entity_auth {"UpdateEntityAuth"};
+
+const string get_read_token_op  {"GetReadToken"};
+const string get_update_token_op {"GetUpdateToken"};
+
+const string add_property_admin {"AddPropertyAdmin"};
+const string update_property_admin {"UpdatePropertyAdmin"};
+
+
+/*
+  Cache of opened tables
+ */
+TableCache table_cache {};
+
+/*
+  Top-level routine for processing all HTTP POST requests.
+ */
+void handle_post(http_request message) {
+  string path {uri::decode(message.relative_uri().path())};
+  cout << endl << "**** POST " << path << endl;
 }
+
+/*
+  Main server routine
+
+  Install handlers for the HTTP requests and open the listener,
+  which processes each request asynchronously.
+  
+  Wait for a carriage return, then shut the server down.
+ */
+int main (int argc, char const * argv[]) {
+  cout << "PushServer Open" << endl;
+  cout << "Parsing connection string" << endl;
+  table_cache.init (storage_connection_string);
+
+  cout << "Opening listener" << endl;
+  http_listener listener {def_url};
+  //listener.support(methods::GET, &handle_get);
+  listener.support(methods::POST, &handle_post);
+  //listener.support(methods::PUT, &handle_put);
+  //listener.support(methods::DEL, &handle_delete);
+  listener.open().wait(); // Wait for listener to complete starting
+
+  cout << "Enter carriage return to stop server." << endl;
+  string line;
+  getline(std::cin, line);
+
+  // Shut it down
+  listener.close().wait();
+  cout << "Closed" << endl;
+}
+
